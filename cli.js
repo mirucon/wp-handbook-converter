4ead1f8903011067bc391636994fc580ab575cf6
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2022-07-28 19:36:55 +0900

    Enhancement: add / fix turndown rules, improve CLI (#43)
    * Enhancement: add / fix turndown rules, improve CLI
    
    * Fix comment typo
    
    Co-authored-by: Toshihiro Kanai <i@miruc.co>
    
    * Code Refactoring
    
    Co-authored-by: Toshihiro Kanai <i@miruc.co>
    
    * Revert CLI description
    
    * Lint fix
    
    Co-authored-by: Toshihiro Kanai <i@miruc.co>

--- cli.js
+++ cli.js
@@ -8,11 +8,121 @@ const { program } = require('commander')
 const mkdirp = require('mkdirp')
 const del = require('del')
 const WPAPI = require('wpapi')
-const turndown = require('turndown')
-const turndownService = new turndown({
+const TurndownService = require('turndown')
+const { tables } = require('turndown-plugin-gfm')
+
+// Languages that can be specified in the code markdown
+const codeLanguages = {
+  css: 'css',
+  bash: 'bash',
+  php: 'php',
+  yaml: 'yaml',
+  xml: 'xml',
+  jscript: 'javascript',
+}
+
+// Rules that remove escapes in code blocks
+const unEscapes = [
+  [/\\\\/g, '\\'],
+  [/\\\*/g, '*'],
+  [/\\-/g, '-'],
+  [/^\\+ /g, '+ '],
+  [/\\=/g, '='],
+  [/\\`/g, '`'],
+  [/\\~~~/g, '~~~'],
+  [/\\\[/g, '['],
+  [/\\\]/g, ']'],
+  [/\\>/g, '>'],
+  [/\\_/g, '_'],
+  [/\&quot;/g, '"'],
+  [/\&lt;/g, '<'],
+  [/\&gt;/g, '>'],
+]
+
+const turndownService = new TurndownService({
   headingStyle: 'atx',
   codeBlockStyle: 'fenced',
   emDelimiter: '*',
+})
+turndownService.use(tables)
+
+// Remove Glossary
+turndownService.addRule('glossary', {
+  filter: (node) => {
+    const classList = node.getAttribute('class')
+    if (classList) {
+      return classList === 'glossary-item-hidden-content'
+    }
+    return false
+  },
+  replacement: () => {
+    return ''
+  },
+})
+
+// Remove code trigger anchor
+turndownService.addRule('code-trigger-anchor', {
+  filter: (node) => {
+    const classList = node.getAttribute('class')
+    if (classList) {
+      return (
+        classList.includes('show-complete-source') ||
+        classList.includes(`less-complete-source`)
+      )
+    }
+    return false
+  },
+  replacement: () => {
+    return ''
+  },
+})
+
+// Transform dt tag to strong tag
+turndownService.addRule('dt-to-strong', {
+  filter: ['dt'],
+  replacement: (content, node, options) => {
+    return options.strongDelimiter + content + options.strongDelimiter
+  },
+})
+
+// Transform pre code block to code markdown
+turndownService.addRule('precode to code', {
+  filter: (node) => {
+    const classList = node.getAttribute('class')
+    const isCode =
+      node.nodeName === 'PRE' && classList && classList.includes('brush:')
+    return isCode
+  },
+  replacement: (content, node) => {
+    const classList = node.getAttribute('class')
+
+    // Search for a language that matches the list of code languages
+    const codeLanguage = Object.keys(codeLanguages).reduce(
+      (currentLanguage, language) => {
+        if (classList.includes(language)) {
+          return codeLanguages[language]
+        }
+        return currentLanguage
+      },
+      undefined
+    )
+
+    // Unescape contents
+    let newContent = unEscapes.reduce((accumulator, unEscape) => {
+      return accumulator.replace(unEscape[0], unEscape[1])
+    }, content)
+
+    // Remove br tag
+    newContent = newContent.replace(/^<br \/>\n\n|<br \/>\n/g, '\n')
+    // Remove first and last paragraph tag
+    newContent = newContent.replace(/^<\/p>|<p>$/g, '')
+    // Remove first new line
+    newContent = newContent.replace(/^\n/, '')
+    // Convert to language-aware markdown
+    newContent = '```' + (codeLanguage ?? '') + '\n' + newContent + '```'
+
+    return newContent
+  },
 })
 
 const getAll = (request) => {
@@ -34,6 +144,7 @@ const generateJson = async (
   outputDir,
   regenerate
 ) => {
+  team = team ? `${team}/` : ''
   handbook = handbook ? handbook : 'handbook'
   subdomain = `${
     subdomain ? (subdomain === 'w.org' ? '' : subdomain) : 'make'
@@ -59,12 +170,13 @@ const generateJson = async (
     })
 
   const wp = new WPAPI({
-    endpoint: `https://${subdomain}wordpress.org/${team}/wp-json`,
+    endpoint: `https://${subdomain}wordpress.org/${team}wp-json`,
   })
+
   wp.handbooks = wp.registerRoute('wp/v2', `/${handbook}/(?P<id>)`)
 
   console.log(
-    `Connecting to https://${subdomain}wordpress.org/${team}/wp-json/wp/v2/${handbook}/`
+    `Connecting to https://${subdomain}wordpress.org/${team}wp-json/wp/v2/${handbook}/`
   )
 
   getAll(wp.handbooks()).then(async (allPosts) => {
@@ -82,18 +194,16 @@ const generateJson = async (
         rootPath = `https://${subdomain}wordpress.org/${team}/${handbook}/`
       }
     }
+
     for (const item of allPosts) {
       const path = item.link.split(rootPath)[1].replace(/\/$/, '') || 'index'
       const filePath =
         path.split('/').length > 1
           ? path.substring(0, path.lastIndexOf('/')) + '/'
           : ''
-      // remove <span class='glossary-item-hidden-content'>inner contents
-      const preContent = item.content.rendered.replace(
-        /<span class=\'glossary-item-hidden-content\'>.*?<\/span><\/span>/g,
-        ''
-      )
-      const markdownContent = turndownService.turndown(preContent)
+
+      const content = item.content.rendered
+      const markdownContent = turndownService.turndown(content)
       const markdown = `# ${item.title.rendered}\n\n${markdownContent}`
 
       await mkdirp(`${outputDir}/${filePath}`)
@@ -155,8 +265,8 @@ const generateJson = async (
 
 program
   .version(packageJson.version)
-  .arguments('<team>')
   .description('Generate a menu JSON file for WordPress.org handbook')
+  .option('-t, --team <team>', 'Specify team name')
   .option(
     '-b, --handbook <handbook>',
     'Specify handbook name (default "handbook")'
@@ -173,9 +283,9 @@ program
     '-r --regenerate',
     'If this option is supplied, the directory you specified as output directory will once deleted, and it will regenerate all the files in the directory'
   )
-  .action((team, options) => {
+  .action((options) => {
     generateJson(
-      team,
+      options.team,
       options.handbook,
       options.subDomain,
       options.outputDir,
